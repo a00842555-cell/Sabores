29456d06a02224420f7d3e602a3979a27b38570a
Erick Manuel Ureña Matos <A00842555@tec.mx>
2026-09-09 11:05:38 -0600

    Checkpoint a3

--- app/src/main/java/mx/tec/sabores/MainActivity.kt
+++ app/src/main/java/mx/tec/sabores/MainActivity.kt
@@ -3,6 +3,8 @@ package mx.tec.sabores
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import mx.tec.sabores.ui.navigation.SaboresApp
 import mx.tec.sabores.ui.theme.SaboresTheme
 
@@ -10,5 +12,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { SaboresTheme { SaboresApp() } }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SaboresTheme()  {
+        SaboresApp()
     }
 }
